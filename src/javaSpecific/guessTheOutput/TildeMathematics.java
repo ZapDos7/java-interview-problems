dfc025dc3cebb9340351eac8d32c474e21b27800
ZapDos7 <jean.darker007@gmail.com>
2025-06-10 21:01:23 +0300

    further explanation of tilde in Java - add one bitwise

--- src/javaSpecific/guessTheOutput/TildeMathematics.java
+++ src/javaSpecific/guessTheOutput/TildeMathematics.java
@@ -1,7 +1,14 @@
 package javaSpecific.guessTheOutput;
 
+//  -x = ~x + 1;
+// -~x = x + 1 (by replacing x by ~x)
 public class TildeMathematics {
     public static void main(String[] args) {
+        System.out.println(~-9);     // 8
+        System.out.println(~-100);   // 99
+        System.out.println(~-1);     // 0
+        System.out.println(~-0);     // -1
+
         System.out.println(~9);     // -10
         System.out.println(~100);   // -101
         System.out.println(~1);     // -2
